e5325d1175f55a3e0f9705f79c0f7b781ce54716
2fefbc1855f66ae7ba9bdd6f7de178e81e3157dd <philip.haynes@me.com>
2020-08-25 09:51:43 +1000

    Updated run cmd

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 source /var/host-paths.txt
 source /var/file-paths.txt
 export SOURCE_HOST SOURCE_PATH LOCAL_PATH
-echo rsync --remove-source-files -zarvh -e ssh $SOURCE_HOST:$SOURCE_PATH $LOCAL_PATH $2>>$1/var/log/r.log
-rsync --remove-source-files -zarvh -e ssh $SOURCE_HOST:$SOURCE_PATH $LOCAL_PATH $2>>$1/var/log/r.log 
+source /var/cmd.txt
+export CMD
+`$CMD`
